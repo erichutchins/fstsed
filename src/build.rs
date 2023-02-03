8ddba199de7f54572f8eeda54125d0a561e0c74c
Eric Hutchins <ehutchins@gmail.com>
2023-02-03 03:19:19 +0000

    progress

--- src/build.rs
+++ src/build.rs
@@ -1,7 +1,7 @@
 use anyhow::{Error, Result};
 use bstr::io::BufReadExt;
 use camino::Utf8PathBuf;
-use fst::raw::{Fst, Output};
+use fst::SetBuilder;
 use serde_json::Value;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
@@ -16,7 +16,7 @@ where
     let mut vals: Vec<String> = Vec::new();
     input.for_byte_line_with_terminator(|line| {
         // TODO: i cant figure out how to transform the std::io::error into anyhow
-        let jsonline = serde_json::from_slice(&line).unwrap_or_else(|_| Value::default());
+        let jsonline = serde_json::from_slice(line).unwrap_or_else(|_| Value::default());
         let keyvalue = jsonline.get(key).and_then(|v| v.as_str()).unwrap_or("");
         vals.push(format!(
             "{}{}{}",
@@ -29,5 +29,16 @@ where
 
     // sort the vector for fst
     vals.sort_unstable();
-    Ok(())
+
+    // create file
+    let wtr = io::BufWriter::new(File::create(output)?);
+    let mut set = SetBuilder::new(wtr)?;
+
+    // insert into set builder
+    vals.iter().for_each(|line| {
+        set.insert(line).expect("could not update fstsed database");
+    });
+
+    // close the fst
+    set.finish().map_err(From::from)
 }
